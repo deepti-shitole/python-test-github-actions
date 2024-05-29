a9d6b76e6dabddaadc232e1b3dd6baada18860d6
Deepti <93430410+deepti-shitole@users.noreply.github.com>
2024-05-29 16:44:01 +0530

    Update test_addition.py

--- src/test_addition.py
+++ src/test_addition.py
@@ -1,3 +1,5 @@
+
+
 # test_addition.py
 
 import unittest
@@ -9,6 +11,15 @@ class TestAddFunction(unittest.TestCase):
 
     def test_add_mixed_numbers(self):
         self.assertEqual(add(1, -1), 0)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+
+    def test_add_zero(self):
+        self.assertEqual(add(0, 0), 0)
+
+    def test_add_large_numbers(self):
+        self.assertEqual(add(1000000, 2000000), 3000000)
 
 if __name__ == '__main__':
     unittest.main()
